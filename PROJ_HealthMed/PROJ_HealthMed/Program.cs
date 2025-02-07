a367cc19cbfb7fd95214c6605470aa49d59b4540
Giuliano Reis Junior <giulianojr@outlook.com>
2025-02-07 17:23:21 -0300

    Update Program.cs

--- PROJ_HealthMed/PROJ_HealthMed/Program.cs
+++ PROJ_HealthMed/PROJ_HealthMed/Program.cs
@@ -7,56 +7,65 @@ using System.Text;
 using Microsoft.Data.SqlClient;
 using System.Data;
 
-
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddAuthentication(options =>
+namespace PROJ_HealthMed
 {
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
+    public class Program
     {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-    };
-});
+        public static void Main(string[] args) 
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
+            // Configuração de autenticação JWT ppln
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                };
+            });
 
-var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-var stringConexao = configuration.GetValue<string>("ConnectionStringSQL");
-// Add services to the container.
-builder.Services.AddSingleton<IDbConnection>((conexao) => new SqlConnection(stringConexao));
-builder.Services.AddSingleton<IPaciente, PacienteRepository>();
-builder.Services.AddSingleton<IMedico, MedicoRepository>();
-builder.Services.AddSingleton<IAgenda, AgendaRepository>();
-builder.Services.AddSingleton<IAgendamento, AgendamentoRepository>();
-builder.Services.AddSingleton<TokenService>();
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+            // Configuração da conexão com o banco de dados
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var stringConexao = configuration.GetValue<string>("ConnectionStrings:DefaultConnection");
 
-var app = builder.Build();
+            // Adicionar serviços ao contêiner
+            builder.Services.AddSingleton<IDbConnection>((conexao) => new SqlConnection(stringConexao));
+            builder.Services.AddSingleton<IPaciente, PacienteRepository>();
+            builder.Services.AddSingleton<IMedico, MedicoRepository>();
+            builder.Services.AddSingleton<IAgenda, AgendaRepository>();
+            builder.Services.AddSingleton<IAgendamento, AgendamentoRepository>();
+            builder.Services.AddSingleton<TokenService>();
+            builder.Services.AddControllers();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+            // Configuração do Swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configuração do pipeline de requisições HTTP
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
