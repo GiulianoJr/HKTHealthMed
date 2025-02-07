35c167bb624e023744ff24518df1e79f0c80edd8
Giuliano Reis Junior <giulianojr@outlook.com>
2025-02-07 19:52:40 -0300

    1
    1

--- PROJ_HealthMed/PROJ_HealthMed/Program.cs
+++ PROJ_HealthMed/PROJ_HealthMed/Program.cs
@@ -15,7 +15,11 @@ namespace PROJ_HealthMed
         {
             var builder = WebApplication.CreateBuilder(args);
 
+<<<<<<< HEAD
             // Configuração de autenticação JWT ppln
+=======
+            // ConfiguraÃ§Ã£o de autenticaÃ§Ã£o JWT ppln
+>>>>>>> a367cc19cbfb7fd95214c6605470aa49d59b4540
             builder.Services.AddAuthentication(options =>
             {
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -35,11 +39,19 @@ namespace PROJ_HealthMed
                 };
             });
 
+<<<<<<< HEAD
             // Configuração da conexão com o banco de dados
             var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
             var stringConexao = configuration.GetValue<string>("ConnectionStrings:DefaultConnection");
 
             // Adicionar serviços ao contêiner
+=======
+            // ConfiguraÃ§Ã£o da conexÃ£o com o banco de dados
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var stringConexao = configuration.GetValue<string>("ConnectionStrings:DefaultConnection");
+
+            // Adicionar serviÃ§os ao contÃªiner
+>>>>>>> a367cc19cbfb7fd95214c6605470aa49d59b4540
             builder.Services.AddSingleton<IDbConnection>((conexao) => new SqlConnection(stringConexao));
             builder.Services.AddSingleton<IPaciente, PacienteRepository>();
             builder.Services.AddSingleton<IMedico, MedicoRepository>();
@@ -48,13 +60,21 @@ namespace PROJ_HealthMed
             builder.Services.AddSingleton<TokenService>();
             builder.Services.AddControllers();
 
+<<<<<<< HEAD
             // Configuração do Swagger
+=======
+            // ConfiguraÃ§Ã£o do Swagger
+>>>>>>> a367cc19cbfb7fd95214c6605470aa49d59b4540
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
+<<<<<<< HEAD
             // Configuração do pipeline de requisições HTTP
+=======
+            // ConfiguraÃ§Ã£o do pipeline de requisiÃ§Ãµes HTTP
+>>>>>>> a367cc19cbfb7fd95214c6605470aa49d59b4540
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
