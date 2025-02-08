d0f1faabb3bf675f30687611230de138d4b4bd69
Brenno Graça <brennosg_3712@hotmail.com>
2025-02-08 01:29:04 -0300

    Rollback Startup.cs

--- PROJ_HealthMed/PROJ_HealthMed/Startup.cs
+++ PROJ_HealthMed/PROJ_HealthMed/Startup.cs
@@ -1,20 +1,6 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-
 namespace PROJ_HealthMed
 {
-    public class Program
+    public class Startup
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
     }
 }
